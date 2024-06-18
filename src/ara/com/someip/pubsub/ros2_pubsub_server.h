9ba685c4b1461009750cc2e33cf425e75dbf4e6c
yeonh0 <nanyeonho@gmail.com>
2024-06-19 05:15:19 +0900

    Init ros2_pubsub_server

---
+++ src/ara/com/someip/pubsub/ros2_pubsub_server.h
@@ -0,0 +1,31 @@
+#ifndef ROS2_PUBSUB_SERVER_H
+#define ROS2_PUBSUB_SERVER_H
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/byte_multi_array.hpp"
+
+namespace ara
+{
+    namespace com
+    {
+        namespace someip
+        {
+            namespace pubsub
+            {
+                class ROS2PubSubServer
+                {
+                public:
+                    ROS2PubSubServer();
+
+                    void PublishMessage(const std::vector<uint8_t>& payload);
+
+                private:
+                    rclcpp::Node::SharedPtr ros_node;
+                    rclcpp::Publisher<std_msgs::msg::ByteMultiArray>::SharedPtr ros_publisher;
+                };
+            }
+        }
+    }
+}
+
+#endif // ROS2_PUBSUB_SERVER_H
