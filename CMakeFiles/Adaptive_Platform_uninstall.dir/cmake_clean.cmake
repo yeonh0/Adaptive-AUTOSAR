e541472e228d2736c4a937c7adf7ac31a0279eb0
yeonh0 <nanyeonho@gmail.com>
2024-06-28 12:53:36 +0900

    Update .gitignore

--- CMakeFiles/Adaptive_Platform_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Adaptive_Platform_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Adaptive_Platform_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
